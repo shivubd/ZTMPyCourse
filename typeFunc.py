06d66394e306ec6205e6aaf3e4040f2ffe82312f
Shivaraj Deyannavar <62126060+shivubd@users.noreply.github.com>
2021-05-25 21:12:43 +0530

    Create typeFunc.py

---
+++ typeFunc.py
@@ -0,0 +1,25 @@
+num1 = 100
+print(type(num1))
+
+num2 = 32.33
+print(type(num2))
+
+num3 = True
+print(type(num3))
+
+num4 = None
+print(type(num4))
+
+num5 = ['2KL18CS122','Shivu',93.76,2000]
+print(type(num5))
+print(type(num5[2]))
+print(type(num5[1:3]))
+
+num6 = ('Shivaraj',19,6,2000)
+print(type(num6))
+print(type(num6[2]))
+print(type(num6[1:3]))
+
+num7 = {'First':'Shivaraj', 'middle':'B', 'Last':'Deyannavar', 'DOY':2000}
+print(type(num7))
+print(type(num7['DOY']))
