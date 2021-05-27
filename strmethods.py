b844fede88ff0b380f7610894ad53243eee8d565
Shivaraj Deyannavar <62126060+shivubd@users.noreply.github.com>
2021-05-28 00:07:43 +0530

    Create strmethods.py

---
+++ strmethods.py
@@ -0,0 +1,20 @@
+#STRING METHODS
+
+usn = 'x2kl18CS122'
+
+#upper()
+print(usn.upper())
+
+#lower()
+print('\n'+usn.lower())
+
+#capitalize
+print('\n'+usn.capitalize())
+
+#find()
+print('\n'+str(usn.find('l18C')))
+print(str(usn.find('l183'))+'\n')
+
+#replace()
+print(usn.replace('kl','KL'))
+print(usn.replace('x',''))
