672f363db25b94963e7915337dca46804d0633a0
Shivaraj Deyannavar <62126060+shivubd@users.noreply.github.com>
2021-05-27 19:15:02 +0530

    Update mathfuncs.py

--- mathfuncs.py
+++ mathfuncs.py
@@ -3,3 +3,11 @@ print(abs(-22.3))
 print(str(round(3.511))+' '+str(round(3.78))+' '+str(round(3.38)))
 
 #and there are many more to refer
+
+#bin() to convert decimals to binary equivalents
+print(bin(6))
+ 
+#converting binary equivalents to decimlas
+print(int(0b110))
+print(float(0b110))
+print(str(0b110))
