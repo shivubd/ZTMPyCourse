41e60d94be5f9739eaedf35ba8a2b0bbe97ff868
Shivaraj Deyannavar <62126060+shivubd@users.noreply.github.com>
2021-05-28 00:08:39 +0530

    Create typeconv.py

---
+++ typeconv.py
@@ -0,0 +1,42 @@
+#Boolean-True,False
+#bool()
+print(bool(0))
+print(bool(1))
+print(bool(34))
+print(bool('True'))
+print(bool(''))
+print(bool('adcac'))
+print()
+
+#Type conversion
+#str()
+print(str(12.213)+' '+str(type(str(12.213))))
+
+#int()
+x = '20'
+x=int(x)+10
+print(x)
+print()
+
+#float()
+x = '20.15'
+x=float(x)+10.85
+print(x)
+print()
+
+#list()
+x='shivaraj'
+print(list(x))
+print()
+
+#tuple()
+x=list(x)
+print(x)
+print(tuple(x))
+print()
+
+#set()
+print(set(x))
+x=tuple(x)
+print(set(x))
+print()
