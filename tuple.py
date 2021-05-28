d916f909dc39fae8187a6de532dd0738c680f19a
Shivaraj Deyannavar <62126060+shivubd@users.noreply.github.com>
2021-05-28 19:15:39 +0530

    Create tuple.py

---
+++ tuple.py
@@ -0,0 +1,19 @@
+#Tuple
+tup = ('Shivaraj',93.76,82.66,2000)
+print(tup[0])
+print(93.76 in tup)
+print()
+#Accessing a tuple is as same Lists
+
+#tuple methods
+#count()
+tu = (1,2,2,3,3,3)
+print(tu.count(2))
+print(tu.count(3))
+print(tu.count(10))
+print()
+
+#index()
+print(tu.index(2))
+print(tu.index(1))
+print()
