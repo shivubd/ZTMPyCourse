45a481b5a95fb335c758d83ff54b91fb6cf178f2
Shivaraj Deyannavar <62126060+shivubd@users.noreply.github.com>
2021-06-23 19:19:28 +0530

    Create range_enum.py

---
+++ range_enum.py
@@ -0,0 +1,19 @@
+#enumerate() - returs the values along with index
+for i,v in enumerate('HelloOoOo'):
+  print(i,v)
+print()
+
+
+#range()
+for i in range(5):
+  print(i)
+print()
+for i in range(6,11):
+  print(i)
+print()
+for i in range(5,0,-1):
+  print(i)
+print()
+for i in range(0,11,2):
+  print(i)
+print()
