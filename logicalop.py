b0214e9888ff8bf59ed968e6ce4549c30505147e
Shivaraj Deyannavar <62126060+shivubd@users.noreply.github.com>
2021-06-23 19:18:44 +0530

    Create logicalop.py

---
+++ logicalop.py
@@ -0,0 +1,16 @@
+#Logical operators
+'''
+and or not 
+'''
+print(True and True)
+print(True and False)
+print(False and False)
+print(False and True)
+print()
+print(True or True)
+print(True or False)
+print(False or False)
+print(False or True)
+print()
+print(not(True))
+print(not(False))
