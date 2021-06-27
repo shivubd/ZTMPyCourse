a4c3590a1785aeed26c7077eb14cbb71d5a99597
Shivaraj Deyannavar <62126060+shivubd@users.noreply.github.com>
2021-06-27 16:20:57 +0530

    Add files via upload

---
+++ packages.py
@@ -0,0 +1,15 @@
+#PACKAGES EXAMPLE
+
+'''
+import shopping.utility
+OR
+from shopping import utility
+OR
+'''
+from shopping.utility import multiply,divide
+print(multiply(2,3))
+print(multiply(None,3))
+print(divide(6,3))
+print(divide(6,0))
+print(divide(6,'shivu'))
+
