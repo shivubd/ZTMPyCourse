e00a9f86d6df74f55e681ef46f9e84ecc59f9720
Shivaraj Deyannavar <62126060+shivubd@users.noreply.github.com>
2021-06-27 16:19:16 +0530

    Add files via upload

---
+++ built-in_modules.py
@@ -0,0 +1,12 @@
+#BUILT-IN MODULES
+
+import random
+import time
+print(random.random())
+print(random.randint(1,10))
+print(random.choice([1,2,3,4,5]))
+my_list = [1,2,3,4,5]
+random.shuffle(my_list)
+print(my_list)
+
+#REFER DOCUMENTATION FOR MORE MODULES
